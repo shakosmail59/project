ca585d1d422638bf72f80ad4b25dfd47ef3fed33
shalva <shakosmail12345@gmail.com>
2022-06-05 00:03:15 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,139 @@
+"use strict";
+
+
+/*
+
+console.log(2);
+
+const a = 
+{
+    ab:50
+};
+
+a.ab = 10;
+
+console.log(a);
+
+
+var name  ="Ivan";
+
+name = "alex";
+console.log(name);
+
+
+ g = 120;
+
+console.log(g);
+
+const vehicleBodyWidth = 4000,
+      vehicleBodyLength = 50500;
+
+console.log("Area of Car :  " + vehicleBodyWidth + ', Length : '+vehicleBodyLength);
+
+// ["vruru" , "ecraasd","asdaad"].map(item => )
+
+// Request
+
+// data
+// response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+//const COLOR_RED = "#F00";
+
+*/
+
+// Javascript Types
+/*
+
+console.log(-4 / 0);
+
+console.log("string" * 9 ); // NaN - NOt a Number
+
+const persone = `Alex asd  asd asdsdsd dsdsd   sd sds   sd s  sd `;
+
+const bool =  true; // false
+
+const nul  = null;
+console.log(nul);
+
+let und ;
+
+console.log(und);
+
+
+const obj = 
+{
+    name: "John",
+    age: 15,
+    IsMarried : false
+};
+
+
+console.log(obj["name"]);
+console.log(obj.age);
+console.log(obj.IsMarried);
+
+
+const arr = ["plum" ," orange" , {} , 6];
+
+console.log(arr[1]);
+
+
+*/
+
+
+// Create Objects and Array
+/*
+//const arr = [1,2,3];
+const arr = ["a","b","c"];
+
+const arrobj = 
+{
+    a : "a",
+    1 : "b",
+    "2" : "c" 
+};
+
+
+
+console.log(arr[1]);
+console.log(arrobj.a);
+console.log(arrobj[1]);
+console.log(arrobj["2"]);
+
+// const obj = 
+// {
+//     a: 1,
+//     b: 2
+// };
+
+const obj = 
+{
+    "Anna" : 500,
+    "Alice" : 800
+};
+
+*/
+
+
+let incr = 10;
+let decr = 10;
+
+
+// incr++ // postfix
+// decr++ // postfix
+
+//++incr; // prefix
+//--decr; // prefix
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+
+console.log(++incr);
+console.log(--decr);
